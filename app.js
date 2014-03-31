b6fe83470cd8865a6be0784abefc3e40c272faad
Alex <aletormat@gmail.com>
2014-03-31 19:31:52 +0200

    Minor

--- app.js
+++ app.js
@@ -51,8 +51,6 @@ if ('development' == app.get('env')) {
 
 require('./routes/routes.js')(app, passport);
 
-//app.get('/', routes.index);
-//app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
