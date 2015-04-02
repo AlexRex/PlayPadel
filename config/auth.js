05ba96a01502360048c434048e6af20555a2f4b5
Alex <aletormat@gmail.com>
2015-04-02 16:14:42 +0200

    To Merge

--- config/auth.js
+++ config/auth.js
@@ -3,8 +3,8 @@
 
 module.exports = {
 	'facebookAuth' : {
-		'clientID' : '493645097425637',
-		'clientSecret': '15aa4be4721f3e5b4bd02fe908c283f3',
+		'clientID' : '****',
+		'clientSecret': '****',
 		'callbackURL': 'http://192.168.30.49:2000/auth/facebook/callback'
 	}
 };
